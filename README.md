511ce23e2bbaa00a1a197ea9ec3dc38df91476f0
emettely <emettely@gmail.com>
2022-09-24 12:18:59 +0100

    Fix issues with parsing non str type arguments, and additional tweaking

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Solar Panels Analysis Tool
+
+## Context
+
+This is a tool developed in Python3 to aid with calculating the number of years
+it will take to breakeven.
+
+## Default values
+
+|Option name|Description|Default Value|
+|---|---|---|
+|`--pv-capacity`|The solar panel system energy generation capacity|4kW|
+|`--gen-kwh`|The maximum energy generated per 1kW solar panel system. This will largely depend on the orientation of the PV panels and the location of the property. For example, a SSW-facing roof in South West England will do better than a North-facing roof in Wales.|1000 kW/h
+|`--gen-percent`|The percentage of how much will actually be generated out of the maximum generated over the year. We default to 50% as we assume if we average out the electricity generation for the whole year, we only generate half of the maximum capacity.| 50%| 
+|`--cost-rate`|The energy charge rate from energy companies. We use the October 2022 value in London.|51.4 p/kWh
+|`--seg-rate`|The SEG (Smart Export Guarantee) rate from energy companies.|5.5 p/kWh
+|`--inverter-cost`|The cost for inverters.|£470|
+|`--pv-cost`|The cost to install PV panels|£5000|
+|`--battery-cost`|The cost to install batteries|£7500|
+|`--ignore-battery`|Ignore batteries: this will ignore the cost of batteries, and assume that you will be exporting to the grid for any electricity generated.|false|
+
 ## Usage
+
 ```shell
 usage: calculate.py [-h] [-k PV_CAPACITY] [-g GEN_KWH] [-p GEN_PERCENT]
                     [-e COST_RATE] [-s SEG_RATE] [-i INVERTER_COST]
@@ -11,7 +32,7 @@ Get solar panel breakeven cost
 optional arguments:
   -h, --help            show this help message and exit
   -k PV_CAPACITY, --pv-capacity PV_CAPACITY
-                        total capacity of solar panels in kilo watts per hour
+                        total capacity of solar panels in kilo watts
                         (default: 4)
   -g GEN_KWH, --gen-kwh GEN_KWH
                         total energy generated per 1kwh PV (default: 1000)
@@ -33,4 +54,42 @@ optional arguments:
                         the cost of the battery/batteries, including
                         installation (default: 7500)
   --ignore-battery IGNORE_BATTERY
-```
+```usage: calculate.py [-h] [-k PV_CAPACITY] [-u USAGE] [-g GEN_KWH]
+                    [-p GEN_PERCENT] [-e COST_RATE] [-s SEG_RATE]
+                    [-i INVERTER_COST] [-c PV_COST] [-b BATTERY_COST]
+                    [-l LOCATION] [--battery] [--no-battery]
+
+Get solar panel breakeven cost
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k PV_CAPACITY, --pv-capacity PV_CAPACITY
+                        total capacity of solar panels in kilo watts (default:
+                        4.8)
+  -u USAGE, --usage USAGE
+                        total electric usage of solar panels in kilo watts per
+                        annum (default: 3680)
+  -g GEN_KWH, --gen-kwh GEN_KWH
+                        total energy generated per 1kwh PV (default: 1000)
+  -p GEN_PERCENT, --gen-percent GEN_PERCENT
+                        the percentage of actual energy generation (default:
+                        90)
+  -e COST_RATE, --cost-rate COST_RATE
+                        charge rate from energy companies in pence per kWh
+                        (default: 36)
+  -s SEG_RATE, --seg-rate SEG_RATE
+                        the SEG rate from energy companies in pence per kWh
+                        (default: 5.5)
+  -i INVERTER_COST, --inverter-cost INVERTER_COST
+                        the cost of the inverter (default: 470)
+  -c PV_COST, --pv-cost PV_COST
+                        the cost of the solar panels, including installation
+                        (default: 7500)
+  -b BATTERY_COST, --battery-cost BATTERY_COST
+                        the cost of the battery/batteries, including
+                        installation (default: 5500)
+  -l LOCATION, --location LOCATION
+                        the location where PV system will be installed
+                        (default: London)
+  --battery
+  --no-battery
--- calculate.py
+++ calculate.py
@@ -1,72 +1,80 @@
 import argparse
+from pprint import pprint
 
 
 # TODO: this could be tailored to more specific areas, orientation, etc
+def calc_gen_per_kwh(location, orientation):
+    return 1000
+
+
 def calc_annual_energy_generated(capacity_in_kwh, gen_per_kwh,
-                                 gen_rate):
+                                 gen_rate, location, orientation="S"):
+    gen_per_kwh = calc_gen_per_kwh(location, orientation)
     return capacity_in_kwh * (gen_per_kwh / 100) * gen_rate
 
 
-def get_annual_net(generated_energy, cost_rate, seg_rate, inverter_cost=None):
-    net_from_energy = (generated_energy * (cost_rate + seg_rate)) / 100
-    if inverter_cost:
-        return net_from_energy - (inverter_cost / 10)
-    else:
-        return net_from_energy
+def get_annual_net(energy_cost_savings, seg_profit, inverter_cost):
+    return energy_cost_savings + seg_profit - (inverter_cost / 10) # annual cost of inverter
 
-
-def get_breakeven_in_years(installation_cost, generated_energy, cost_rate,
-                           seg_rate, inverter_cost=None):
-    annual_net = get_annual_net(generated_energy, cost_rate, seg_rate,
-                                inverter_cost)
+def get_breakeven_in_years(installation_cost, annual_net):
     return installation_cost / annual_net
 
+def calc_roi_percent(gross, n_years, cost):
+    roi_solar = (gross * n_years - cost) / cost
+    roi_investment = (((1 + roi_solar)**(1 / n_years)) - 1) * 100
+    return roi_investment
 
-def fmt_tmpl_no_batt(breakeven, cost_of_installation, pv_cap, annual_energy_gen,
-                     cost_rate):
+def fmt_tmpl_w_batt(breakeven, cost_of_installation, pv_cap, annual_energy_gen, annual_energy_used, annual_energy_exported,
+                    seg_rate, cost_rate, annual_net):
+    annual_profit = annual_net + annual_energy_exported
     return f"""
-It will take around {round(breakeven)} years to breakeven.
-Solar panels\t\t\t£{cost_of_installation} 
-Solar Panel capacity\t\t{pv_cap} kWh 
-Potentially generated kWs\t{annual_energy_gen}
-10/22 energy price rates\t{cost_rate} p/kwh
-    """
+It will take around {round(breakeven)} years to breakeven based on 10/22 energy price rates {cost_rate} p/kwh.
 
-
-def fmt_tmpl_w_batt(breakeven, cost_of_installation, pv_cap, annual_energy_gen,
-                    seg_rate, cost_rate):
-    return f"""
-It will take around {round(breakeven)} years to breakeven.
 Batteries and Solar panels\t£{cost_of_installation} 
-Solar Panel capacity\t\t{pv_cap} kWh 
-Potentially generated kWs\t{annual_energy_gen}
+Solar Panel capacity\t\t{pv_cap} kW 
 SEG rates\t\t\t{str(seg_rate)} p/kWh
-10/22 energy price rates\t{cost_rate} p/kwh
+
+Yearly
+================
+\tGenerated\t\t{annual_energy_gen} kWh
+-\tSelf-consumption\t{annual_energy_used} kWh
+------------------------------------
+=\tExported\t\t{annual_energy_gen - annual_energy_used} kWh
+
+\tEnergy Savings\t\t£{round(annual_net)}
++\tSEG Profit\t\t£{round(annual_energy_exported)}
+-------------------------------------
+=\tAnnual Net\t\t£{round(annual_profit)}
+
+ROI
+================
+\t\t\t\t{round(calc_roi_percent(annual_profit, 25, cost_of_installation))} %
+
     """
 
 
 def main(cmd_args):
-    annual_energy_generated = calc_annual_energy_generated(
-        cmd_args.pv_capacity, cmd_args.gen_kwh, cmd_args.gen_percent)
+    annual_energy_gen_kwh = calc_annual_energy_generated(
+        cmd_args.pv_capacity, cmd_args.gen_kwh, cmd_args.gen_percent,
+        cmd_args.location)
     cost_of_installation = cmd_args.pv_cost
-    if cmd_args.ignore_battery:
-        breakeven = get_breakeven_in_years(cmd_args.pv_cost,
-                                           annual_energy_generated,
-                                           cmd_args.cost_rate,
-                                           cmd_args.seg_rate)
-        tmpl = fmt_tmpl_no_batt(breakeven, cost_of_installation,
-                                cmd_args.pv_capacity, annual_energy_generated,
-                                cmd_args.cost_rate)
+    pprint(cmd_args)
+    if not cmd_args.battery:
+        usage_percent = 0.35
+
     else:
+        usage_percent = 0.72
         cost_of_installation += cmd_args.battery_cost
-        breakeven = get_breakeven_in_years(
-            cost_of_installation,
-            annual_energy_generated,
-            cmd_args.cost_rate, 0,
-            cmd_args.inverter_cost)
-        tmpl = fmt_tmpl_w_batt(breakeven, cost_of_installation,
-                               cmd_args.pv_capacity, annual_energy_generated,
-                               cmd_args.seg_rate, cmd_args.cost_rate)
+
+    annual_self_consumption_kwh = usage_percent * cmd_args.usage
+    annual_cost_savings_gbp = annual_self_consumption_kwh * cmd_args.cost_rate / 100
+    gross_energy_kwh = annual_energy_gen_kwh - annual_self_consumption_kwh
+    annual_export_profit_gbp = (gross_energy_kwh * cmd_args.seg_rate) / 100
+    annual_net_gbp = get_annual_net(annual_cost_savings_gbp, annual_export_profit_gbp, cmd_args.inverter_cost)
+    breakeven = get_breakeven_in_years(cost_of_installation, annual_net_gbp)
+    tmpl = fmt_tmpl_w_batt(breakeven, cost_of_installation,
+                            cmd_args.pv_capacity, annual_energy_gen_kwh, annual_self_consumption_kwh, annual_export_profit_gbp,
+                            cmd_args.seg_rate, cmd_args.cost_rate, annual_net_gbp)
     print(tmpl)
 
 
@@ -74,34 +82,41 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Get solar panel breakeven cost',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-k', '--pv-capacity', default=4,
-                        help="total capacity of solar panels in kilo watts per hour"
+    parser.add_argument('-k', '--pv-capacity', default=4.8, type=float,
+                        help="total capacity of solar panels in kilo watts"
                         )
-    parser.add_argument('-g', '--gen-kwh', default=1000,
+    parser.add_argument('-u', '--usage', default=3680, type=float,
+                        help="total electric usage of solar panels in kilo watts per annum"
+                        )
+    parser.add_argument('-g', '--gen-kwh', default=1000, type=int,
                         help="total energy generated per 1kwh PV"
                         )
-    parser.add_argument('-p', '--gen-percent', default=50,
+    parser.add_argument('-p', '--gen-percent', default=90, type=int,
                         help="the percentage of actual energy generation",
                         )
     parser.add_argument('-e', '--cost-rate',
-                        default=50,
+                        default=28, type=float,
                         help="charge rate from energy companies in pence per kWh"
                         )
     parser.add_argument('-s', '--seg-rate',
-                        default=5.5,
+                        default=5.5, type=float,
                         help="the SEG rate from energy companies in pence per kWh"
                         )
-    parser.add_argument('-i', '--inverter-cost',
+    parser.add_argument('-i', '--inverter-cost', type=int,
                         default=470, help="the cost of the inverter"
                         )
-    parser.add_argument('-c', '--pv-cost',
-                        default=5000, help="the cost of the solar panels, "
+    parser.add_argument('-c', '--pv-cost', type=int,
+                        default=7500, help="the cost of the solar panels, "
                                            "including installation"
                         )
-    parser.add_argument('-b', '--battery-cost',
-                        default=7500, help="the cost of the battery/batteries, "
+    parser.add_argument('-b', '--battery-cost', type=int,
+                        default=5500, help="the cost of the battery/batteries, "
                                            "including installation"
                         )
-    parser.add_argument('--ignore-battery', default=False)
+    parser.add_argument('-l', '--location',
+                        default='London', help="the location where PV system "
+                                               "will be installed")
+    parser.add_argument('--battery', action='store_true')
+    parser.add_argument('--no-battery', dest='battery', action='store_false')
     args = parser.parse_args()
     main(args)
